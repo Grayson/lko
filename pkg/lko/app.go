f7fda86b275f52cc60b8c77cd660dcadeba9025a
Grayson Hansard <grayson.hansard@gmail.com>
2024-10-14 15:20:02 -0400

    Test building Docker image

--- pkg/lko/app.go
+++ pkg/lko/app.go
@@ -1,5 +1,15 @@
 package lko
 
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+)
+
 type App struct {
 }
 
@@ -8,6 +18,37 @@ func InitApp() *App {
 }
 
 func (app *App) Run() error {
+	docker, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+
+	opts := types.ImageBuildOptions{
+		Tags:   []string{"lko"},
+		Labels: map[string]string{},
+	}
+
+	build, err := archive.TarWithOptions("../../test/server", &archive.TarOptions{})
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	response, err := docker.ImageBuild(
+		ctx,
+		build,
+		opts,
+	)
+	if err != nil {
+		return err
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
+	defer response.Body.Close()
 	println("running")
 	return nil
 }
